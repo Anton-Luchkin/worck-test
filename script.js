c1892be0652f31a9c360da6b6eb6414beeb7d903
Антон Лучкин <luchkin1988@gmail.com>
2020-07-12 01:26:10 +0300

    second

--- script.js
+++ script.js
@@ -1,31 +1,27 @@
 
 const slider = document.querySelector('.slider-wrapper');
+const sliderGG = document.querySelector('.slider');
 const btnFirstSlide = document.querySelector('.first-btn');
 const btnSecondSlide = document.querySelector('.second-btn');
 const btnThirdSlide = document.querySelector('.third-btn');
 
-btnFirstSlide.addEventListener('click', onFirstSlide)
+btnFirstSlide.addEventListener('click', onFirstSlide);
 btnSecondSlide.addEventListener('click', onSecondSlide);
 btnThirdSlide.addEventListener('click', onThirdSlide);
 
 function onFirstSlide() {
-   slider.scroll({
-      left: 0,
-      behavior: 'smooth',
-   });
+   sliderGG.classList.remove('midle');
+   sliderGG.classList.remove('right');
 }
 
 function onSecondSlide() {
-   slider.scroll({
-      left: 450,
-      behavior: 'smooth',
-   });
+   sliderGG.classList.add('midle');
+   sliderGG.classList.remove('right');
+
 }
 
 function onThirdSlide() {
-   slider.scroll({
-      left: 900,
-      behavior: 'smooth',
-   });
+   sliderGG.classList.add('right');
+   sliderGG.classList.remove('midle');
 }
 
